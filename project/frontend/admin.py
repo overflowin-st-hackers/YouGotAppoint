4178b71c5afb968ae1a56e1751665a8d9e8f2596
Saurabh Bansal <b17059@students.iitmandi.ac.in>
2019-04-06 14:31:45 +0530

    models created

--- project/frontend/admin.py
+++ project/frontend/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Specialization, Doctor
 # Register your models here.
+admin.site.register(Doctor)
+admin.site.register(Specialization)
--- project/frontend/models.py
+++ project/frontend/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+    name = models.CharField(max_length=100)
+    rating = models.FloatField(default=0.0)
+    degrees = models.CharField(max_length=300, null=True)
+    no_of_reviews = models.IntegerField(default=0)
+    contact_no = models.BigIntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Specialization(models.Model):
+    disease = models.CharField(max_length=100)
+    doctor = models.ForeignKey(Doctor, related_name='specializations', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.disease
+
